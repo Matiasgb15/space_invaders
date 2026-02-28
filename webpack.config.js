0c640f12c98e14ed31c1034020e46f600e3becd3
Matias Gonzalez Bonilla <mgonzalezb01@libertadores.edu.co>
2026-02-28 20:36:08 +0000

    modificacion de  imagen de enemigos

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
+const path = require('path');
+
 module.exports = {
   entry: "./lib/space_invaders.js",
   output: {
-  	filename: "./bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+    ,
+    publicPath: '/dist/'
   },
   devtool: 'source-map'
+  ,
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '.')
+    },
+    open: true,
+    port: 8080
+  }
 };
